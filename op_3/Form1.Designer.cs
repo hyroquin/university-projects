3bb16d5b4126c1bf6056da0efb49cb12e7d22c73
hyroquin <hyroquin@gmail.com>
2019-01-23 23:08:40 +0300

    Fixed typo

--- op_3/Form1.Designer.cs
+++ op_3/Form1.Designer.cs
@@ -58,9 +58,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(263, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(219, 24);
+            this.label1.Size = new System.Drawing.Size(225, 24);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Input x and y coodinates:";
+            this.label1.Text = "Input x and y coordinates:";
             // 
             // label2
             // 
